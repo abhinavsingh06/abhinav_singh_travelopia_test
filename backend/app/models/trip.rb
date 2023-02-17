26288f41cde4a759c0878b739865e7d468425600
abhinav <singh.abhinav6996@gmail.com>
2023-02-17 17:34:32 +0530

    fix: add validation for enum

--- backend/app/models/trip.rb
+++ backend/app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   enum country: [:india, :africa, :europe]
-  validates :country, presence: true, inclusion: country.keys 
+  validates :country, presence: true
   validates :no_of_person, presence: true
   validates :budget_per_person, presence: true
 end
