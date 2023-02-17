430e2e9982c4cca6785a373e7d7a4500d4f3aef1
abhinav <singh.abhinav6996@gmail.com>
2023-02-17 15:57:03 +0530

    chore: add routes for actions

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :submissions, only: [:index, :create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    end
+  end
+  root "api/v1/submissions#index"
 end
