fc0d694f065b6474334537dcabc7e0078de3ab90
abhinav <singh.abhinav6996@gmail.com>
2023-02-17 17:19:48 +0530

    fix: change field name

--- backend/db/migrate/20230217093405_create_trips.rb
+++ backend/db/migrate/20230217093405_create_trips.rb
@@ -1,10 +1,10 @@
 class CreateTrips < ActiveRecord::Migration[7.0]
   def change
     create_table :trips do |t|
-      t.integer :place, default: 0, null: false
+      t.integer :country, default: 0, null: false
       t.integer :no_of_person, default: 1, null: false
       t.integer :budget_per_person, null: false
-      t.references :traveler, null: false
+      t.references :user, null: false
       t.timestamps
     end
   end
