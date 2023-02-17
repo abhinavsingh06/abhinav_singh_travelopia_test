6995db288eb188de5f5d8538723b3342c8a214a8
abhinav <singh.abhinav6996@gmail.com>
2023-02-17 15:14:26 +0530

    chore: add trip model validations

--- backend/app/models/trip.rb
+++ backend/app/models/trip.rb
@@ -1,2 +1,7 @@
 class Trip < ApplicationRecord
+  belongs_to :user, foreign_key: :user_id
+  enum place: [:india, :africa, :europe]
+  validates :place, presence: true, inclusion: place.keys 
+  validates :no_of_person, presence: true
+  validates :budget_per_person, presence: true
 end
